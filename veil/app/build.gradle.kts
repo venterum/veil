c75b461b2d4e441d8b8678d2e29d99eb825542ac
venterum <evgenij@venterum.com>
2026-09-14 02:41:55 +0300

    bump version to 0.6.2

--- veil/app/build.gradle.kts
+++ veil/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.vntrum.veil"
         minSdk = 24
         targetSdk = 37
-        versionCode = 11
-        versionName = "0.6.1"
+        versionCode = 12
+        versionName = "0.6.2"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
